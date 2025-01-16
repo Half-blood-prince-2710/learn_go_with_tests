7ad9506c3d7fd60c1fff17fa132dcaac10243053
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-01-16 16:37:41 +0530

    testing and bencharming with strings.builder or string concatenation

--- GO_FUNDAMENTALS/Iteration/iteration.go
+++ GO_FUNDAMENTALS/Iteration/iteration.go
@@ -1,5 +1,7 @@
 package iteration
 
+import "strings"
+
 
 
 
@@ -7,10 +9,10 @@ func Repeat(char string, count int) string {
 	if count<0 {
 		return "Repeat count should be greater than equal to 0"
 	}
-	var repeated string
+	var repeated strings.Builder
 	for i:=0; i<count; i++ {
-		repeated += char
+		repeated.WriteString(char)
 	}
-	return repeated
+	return repeated.String()
 }
 
