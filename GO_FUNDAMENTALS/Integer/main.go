3fcc9d614663d2fbfdf3efdd3b41bfc7b234fb11
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-01-16 15:52:53 +0530

    removing main func

--- GO_FUNDAMENTALS/Integer/main.go
+++ GO_FUNDAMENTALS/Integer/main.go
@@ -1,6 +1,6 @@
 package integer
 
-import "fmt"
+
 
 
 // Add takes two integers and returns the sum of them
@@ -8,7 +8,3 @@ func Add(x int, y int) int {
 	return x+y
 }
 
-func main(){
-	fmt.Println(Add(1,2))
-	// Output: 5
-}
