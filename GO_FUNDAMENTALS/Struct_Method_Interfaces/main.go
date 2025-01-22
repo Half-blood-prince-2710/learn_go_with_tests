54b0910983ffe41e03a8ca0b197d6cda0e8d89c0
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-01-22 13:19:35 +0530

    Completed structs methods and interfaces

--- GO_FUNDAMENTALS/Struct_Method_Interfaces/main.go
+++ GO_FUNDAMENTALS/Struct_Method_Interfaces/main.go
@@ -1,6 +1,6 @@
 package main
 
-// import "math"
+import "math"
 
 // type Rectangle struct {
 // 	Width float64
@@ -11,36 +11,36 @@ package main
 // 	radius float64
 // }
 
-// func Perimeter(r Rectangle) float64 {
-// 	return 2 * (r.Height+r.Width)
-// }
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.Height+r.Width)
+}
 
-// // func Area(r Rectangle) float64 {
-// // 	return r.Height * r.Width
-// // }
-// type Shape interface {
-// 	Area() float64
+// func Area(r Rectangle) float64 {
+// 	return r.Height * r.Width
 // }
+type Shape interface {
+	Area() float64
+}
 
-// type Rectangle struct {
-// 	Width  float64
-// 	Height float64
-// }
-// type Triangle struct {
-// 	Base   float64
-// 	Height float64
-// }
-// func (r Rectangle) Area() float64 {
-// 	return r.Width * r.Height
-// }
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
 
-// type Circle struct {
-// 	Radius float64
-// }
+type Circle struct {
+	Radius float64
+}
 
-// func (c Circle) Area() float64 {
-// 	return math.Pi * c.Radius * c.Radius
-// }
-// func (t Triangle) Area() float64 {
-// 	return (t.Base * t.Height) * 0.5
-// }
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
